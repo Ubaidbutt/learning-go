776b1636733a5f43c311b4f2d989993bb2d49a4a
Ubaid R Butt <ubaidbutt@Ubaids-Laptop.local>
2024-03-19 21:42:47 +0100

    Fix the main function

--- calculator/main.go
+++ calculator/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"ubaidbutt.com/maths/calculator/division"
+	"ubaidbutt.com/maths/calculator/multiplication"
+	"ubaidbutt.com/maths/calculator/subtraction"
 	"ubaidbutt.com/maths/calculator/sum"
 )
 
@@ -11,6 +13,8 @@ func main() {
 	const a = 10
 	const b = 50
 
-	fmt.Println("The sum of the two values is: ", sum.Add(a, b))
-	fmt.Println("The sum of the two values is: ", division.Divide(b, a))
+	fmt.Println("The sum result produces: ", sum.Add(a, b))
+	fmt.Println("The division result produces: ", division.Divide(b, a))
+	fmt.Println("The subtraction result produces: ", subtraction.Subtract(b, a))
+	fmt.Println("The multiplication result produces : ", multiplication.Multiply(a, b))
 }
